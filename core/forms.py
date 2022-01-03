157c98b397c5cdd9981bd3c263b5b050e88851f4
marlo <mark.datsuk@gmail.com>
2022-01-03 11:06:05 +0300

    change passowrd fixes

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import SetPasswordForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -17,4 +21,49 @@ class UpdateUserForm(CreateUserForm):
 
     class Meta(CreateUserForm.Meta):
         pass
+
+
+class PasswordChangeForm(forms.Form):
+    newpass1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(),
+    )
+    newpass2 = forms.CharField(
+        label=_("New password confirmation"),
+        widget=forms.PasswordInput(),
+    )
+
+    # Конструктор класса, мы его переписываем что-бы иметь возможность в форму передать юзера
+    # Юзера надо добавить в контекст self до вызова super, потому что в родительском классе его нет и вылетит эксепшн
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+
+    # В этом методе мы делаем все валидации формы
+    # https://docs.djangoproject.com/en/4.0/ref/forms/validation/
+    # Официальная дока, ну или найди себе какой видос/мануал, про валидацию форм.
+    def clean(self, *args, **kwargs):
+        print(args, kwargs)
+        password1 = self.cleaned_data.get('newpass1')
+        password2 = self.cleaned_data.get('newpass2')
+        if password1 and password2:
+            if password1 != password2:
+                raise ValidationError(
+                    _('The two password fields didn’t match.')
+                )
+        return self.cleaned_data
+
+    # Этот метод небходим для сохранения данных формы в базу
+    # Его ты вызываешь во въюхе.
+    def save(self, commit=True):
+        password = self.cleaned_data["newpass1"]
+        # set_password это стандартный метод модели юзера (где-то в родительских классах он описан).
+        # Он хэширует пароль необратимым (односторонним) шифрованием, чтобы в базе лежал только его хеш.
+        # Это используется для защиты пользователей, чтоюы даже адмены сайта не могли зайти в аккаунт юзера, ну точнее
+        # не знали пароль, зайти то они скорей всего смогут=))
+        # https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#django.contrib.auth.models.AbstractBaseUser.set_password
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
 
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from core import models
+from core.forms import PasswordChangeForm
 
 
 def index(request):
@@ -7,8 +9,22 @@ def index(request):
 
 
 def change_password(request, pk):
-    form = PasswordChangeForm(request.POST or None)
+    # Принтуй, что тебе приходит в запросе
+    print(request.POST)
+    # Получаем объект юзера или говорим серверу кинуть 404 HTTP ощибку, если объект не найден
+    user = get_object_or_404(models.User, pk=pk)
+    # Передаем юзера форме, а также направляем туда все данные HTTP метода POST, если они прилетели
+    form = PasswordChangeForm(request.POST or None, user=user)
+    # Т.к. в этой вьюхе обрабатывается два метода, GET и POST, их нужно отделять
+    # Поэтому логику POST, при условии валидности формы, отравляем в иф
+    # Смотри форму forms.PasswordChangeForm метод clean, там будут валидации, которые стартуют при вызове метода
+    # form.is_valid()
+    #
+    # GET запрос приходит на сервер по умолчанию, а вот POST надо послать формой HTML
+    # Это одна из немногих штук, как HTML влияет на радоту сервера (ито если прогрпммист это обработает)
+    print(request.method == 'POST', form.is_valid())
     if request.method == 'POST' and form.is_valid():
+        print('valid:', user)
         form.save()
         return redirect('admin:users_update', pk)
     return render(request, 'core/users/change_password.html', locals())
