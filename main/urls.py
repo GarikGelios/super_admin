aa78bddfe2a629351c3b71297735081d4b1a9ea5
temparacha <temparacha@gmail.com>
2022-02-09 14:55:22 +0300

    public posts page added

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,10 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='main'),
+
+
+    path('posts/', ListView.as_view(
+        model=models.Post,
+        template_name='main/posts/list.html'
+    ), name='posts_list'),
 ]
