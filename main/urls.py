16c64e2a1e997ffac0f11fbc427d18500c1ead28
marlo <mark.datsuk@gmail.com>
2022-02-24 12:36:48 +0300

    refactor public posts

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from main import views, models
 from django.views.generic import ListView, CreateView, UpdateView
-from main.views import PublishedPostsView
 
 
 app_name = 'main'
@@ -10,8 +9,8 @@ urlpatterns = [
     path('', views.index, name='main'),
 
 
-    path('posts/', PublishedPostsView.as_view(
-        model=models.Post,
+    path('posts/', ListView.as_view(
+        queryset=models.Post.objects.filter(is_published=True),
         template_name='main/posts/list.html'
     ), name='posts_list'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,10 +5,3 @@ from core.models import Post
 
 def index(request):
     return render(request, 'main/index.html', locals())
-
-
-class PublishedPostsView(ListView):
-
-    context_object_name = 'published_posts_list'
-    queryset = Post.objects.filter(is_published=True)
-    template_name = '/posts/list.html'
